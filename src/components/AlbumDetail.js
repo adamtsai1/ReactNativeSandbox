27618c3bfda80772352d0c039e0a0b64312c9b97
Adam Tsai <adam@visualchefs.com>
2018-04-13 11:16:44 -0400

    .

--- src/components/AlbumDetail.js
+++ src/components/AlbumDetail.js
@@ -1,19 +1,79 @@
 import React, { Component } from 'react';
-import { Text } from 'react-native';
+import { Image, Linking, Text, View } from 'react-native';
 import PropTypes from 'prop-types';
 
 import Card from './Card';
+import CardSection from './CardSection';
+import Button from './Button';
 
-export default class AlbumDetail extends Component {
-    static propTypes = {
-        album: PropTypes.object.isRequired
-    };
+const AlbumDetail = ({ album }) => {
+    const {
+        artist,
+        image,
+        thumbnail_image,
+        title,
+        url,
+    } = album;
 
-    render() {
-        return (
-            <Card>
-                <Text>{this.props.album.title}</Text>
-            </Card>
-        );
-    }
-}
+    const {
+        headerContentStyle,
+        headerTextStyle,
+        imageStyle,
+        thumbnailStyle,
+        thumbnailContainerStyle,
+    } = styles;
+
+    return (
+        <Card>
+            <CardSection>
+                <View style={thumbnailContainerStyle}>
+                    <Image
+                        source={{ uri: thumbnail_image }}
+                        style={thumbnailStyle}
+                    />
+                </View>
+                <View style={headerContentStyle}>
+                    <Text style={headerTextStyle}>{title}</Text>
+                    <Text>{artist}</Text>
+                </View>
+            </CardSection>
+            
+            <CardSection>
+                <Image source={{ url: image }} style={imageStyle} />
+            </CardSection>
+
+            <CardSection>
+                <Button onPress={() => Linking.openURL(url)}>
+                    Buy Now
+                </Button>
+            </CardSection>
+        </Card>
+    );
+};
+
+const styles = {
+    headerContentStyle: {
+        flexDirection: 'column',
+        justifyContent: 'space-around',
+    },
+    headerTextStyle: {
+        fontSize: 18,
+    },
+    thumbnailStyle: {
+        height: 50,
+        width: 50,
+    },
+    thumbnailContainerStyle: {
+        justifyContent: 'center',
+        alignItems: 'center',
+        marginLeft: 10,
+        marginRight: 10,
+    },
+    imageStyle: {
+        height: 300,
+        flex: 1,
+        width: null,
+    },
+};
+
+export default AlbumDetail;
